2d22c7841ca0d880836fbafdf66c613104332caa
Yurii Piets <yurii.piets@gmail.com>
2018-10-09 10:54:46 +0200

    Get response from weather service

--- weatherapp/ViewController.swift
+++ weatherapp/ViewController.swift
@@ -12,9 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let locationId = "523920";
+        let weatherUrl = "https://www.metaweather.com/api/location/" + locationId;
+        
+        let url = URL(string: weatherUrl);
+        let request: URLRequest = URLRequest(url: url!);
+        let dataTask = URLSession.shared.dataTask(with: request) {(d, resp, err) in
+            
+            if let err = err {
+                print("Unexpected \(err)");
+                return;
+            }
+            
+            let json = String(data: d!, encoding: String.Encoding.utf8);
+            print(json);
+        };
+        
+        
+        dataTask.resume();
     }
 
 
 }
-
